ade5d29708c8bbcb6a18af80848fc33cfa857071
unknown <bary.berdiyev.97@mail.ru>
2022-12-27 08:37:44 +0700

    компонент страницы для неавторизованных пользователей

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+/**
+ * компонент страницы для неавторизованных пользователей
+ */
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}
